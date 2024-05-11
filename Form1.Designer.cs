4c0bd490a5d90f3dbd1fd007ce40026d0bcd918f
mika huotari <mika.huotari@edu.keuda.fi>
2024-05-11 11:05:36 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace TurboBoost
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.boostLB = new System.Windows.Forms.Label();
+            this.connectBT = new System.Windows.Forms.Button();
+            this.comPortBTN = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // boostLB
+            // 
+            this.boostLB.AutoSize = true;
+            this.boostLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boostLB.Location = new System.Drawing.Point(219, 34);
+            this.boostLB.Name = "boostLB";
+            this.boostLB.Size = new System.Drawing.Size(59, 25);
+            this.boostLB.TabIndex = 0;
+            this.boostLB.Text = "value";
+            // 
+            // connectBT
+            // 
+            this.connectBT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connectBT.Location = new System.Drawing.Point(203, 139);
+            this.connectBT.Name = "connectBT";
+            this.connectBT.Size = new System.Drawing.Size(91, 36);
+            this.connectBT.TabIndex = 1;
+            this.connectBT.Text = "Hae";
+            this.connectBT.UseVisualStyleBackColor = true;
+            this.connectBT.Click += new System.EventHandler(this.connectBT_Click);
+            // 
+            // comPortBTN
+            // 
+            this.comPortBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comPortBTN.Location = new System.Drawing.Point(203, 205);
+            this.comPortBTN.Name = "comPortBTN";
+            this.comPortBTN.Size = new System.Drawing.Size(91, 37);
+            this.comPortBTN.TabIndex = 2;
+            this.comPortBTN.Text = "Valitse";
+            this.comPortBTN.UseVisualStyleBackColor = true;
+            this.comPortBTN.Click += new System.EventHandler(this.comPortBTN_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Valitse Com portti";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(46, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Hae data";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(34, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(203, 80);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(91, 40);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(67, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Bar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 267);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comPortBTN);
+            this.Controls.Add(this.connectBT);
+            this.Controls.Add(this.boostLB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label boostLB;
+        private System.Windows.Forms.Button connectBT;
+        private System.Windows.Forms.Button comPortBTN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TurboBoost
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort = new SerialPort();
+            serialPort.BaudRate = 38400;
+            serialPort.DataReceived += SerialPort_DataReceived;
+            comboBox1.Items.AddRange(new string[] { "9600", "10400", "38400", "250000", "500000" });
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = serialPort.ReadLine();
+
+            // Tarkista, että vastaanotettu data ei ole tyhjä
+            if (!string.IsNullOrEmpty(data))
+            {
+                // Erotetaan merkkijono pilkkujen kohdalta ja valitaan ensimmäinen kenttä
+                string turboBoostValue = data.Split(',')[0];
+
+                // Päivitetään labelin arvo uudella Turbo Boost -arvolla
+                UpdateTurboBoostValue(turboBoostValue);
+            }
+        }
+        private void UpdateTurboBoostValue(string value)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(UpdateTurboBoostValue), value);
+                return;
+            }
+
+            double turboBoostVolts = double.Parse(value);
+            double minVolts = 0.5;
+            double maxVolts = 4.5;
+            double minPidValue = 0;
+            double maxPidValue = 100;
+
+            // Muunna volttiarvo pid-arvoksi
+            double pidValue = ((turboBoostVolts - minVolts) / (maxVolts - minVolts)) * (maxPidValue - minPidValue) + minPidValue;
+
+            // Aseta ProgressBarin arvo
+            progressBar1.Value = (int)pidValue;
+
+            boostLB.Text = "Turbo Boost: " + value + " V"; // Päivitä labelin teksti
+        }
+
+
+        private void connectBT_Click(object sender, EventArgs e)
+        {
+            if (!serialPort.IsOpen)
+            {
+                try
+                {
+                    serialPort.PortName = "COM3"; // Määritä oikea COM-portti 
+                    serialPort.Open();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error opening serial port: " + ex.Message);
+                }
+            }
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+        }
+
+        private void comPortBTN_Click(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            if (ports.Length > 0)
+            {
+                //  ComboBox, joka sisältää COM-portit
+                System.Windows.Forms.ComboBox comboBox = new System.Windows.Forms.ComboBox();
+                comboBox.Items.AddRange(ports);
+                comboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+
+              
+                DialogResult result = MessageBox.Show(comboBox, "Valitse COM-portti:", "COM-portin valinta", MessageBoxButtons.OKCancel);
+
+                if (result == DialogResult.OK)
+                {
+                    // Tarkistaa että käyttäjä on valinnut COM-portin
+                    if (comboBox.SelectedItem != null)
+                    {
+                        // Aseta valittu COM-portti sarjaportin nimeksi
+                        serialPort.PortName = comboBox.SelectedItem.ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Et valinnut COM-porttia.");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ei löydetty käytettävissä olevia COM-portteja.");
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serialPort.BaudRate = int.Parse(comboBox1.SelectedItem.ToString());
+        }
+    }
+}
